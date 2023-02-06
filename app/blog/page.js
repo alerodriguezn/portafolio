cba6d6b75b6c325f701ec020a0f4b13acd8619e8
alerodriguezn <navarrocmo@gmail.com>
2023-02-06 14:56:01 -0600

    Webpack markdown loader

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,21 +1,15 @@
 
-import axios from "axios";
-import ListPosts from "@/components/ListPosts";
 
-async function getData() {
-  const { data } = await axios('http://localhost:3000/api/blog', { cache: 'no-store' });
-  return data;
-}
-const BlogPage = async  () => {
-  
-  const posts = await getData();
+const BlogPage = async () => {
+
   return (
-
     <main className="mt-14">
-      <h2 className="text-white font-bold text-2xl border-b border-zinc-600 pb-2">Blog</h2>
-      <ListPosts posts={posts} />
+      <h2 className="text-white font-bold text-2xl border-b border-zinc-600 pb-2">
+        Blog
+      </h2>
+  
     </main>
-  )
-}
+  );
+};
 
-export default BlogPage
+export default BlogPage;
