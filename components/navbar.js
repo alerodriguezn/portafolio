ccbb5e5638adb4d96bd0ffffbae3ea5b2e5d484c
alerodriguezn <navarrocmo@gmail.com>
2023-02-26 22:51:19 -0600

    Update next config and nav

--- components/navbar.js
+++ components/navbar.js
@@ -8,10 +8,12 @@ export const Navbar = () => {
     <header className="mt-8 flex flex-col justify-center items-center md:flex-row md:justify-between w-full">
         <h2 className="text-white font-bold mb-3 md:mb-0"> <span className="text-[#6bd968]">{'{ '}</span> Ale Rodríguez<span className="text-[#6bd968]">{' }'}</span></h2>
         <div className="flex md:gap-4 gap-1">
-          <Link className={`text-white font-bold rounded-md px-2 hover:bg-zinc-800 ${pathname === "/" ? 'text-gray-500': ''} `} href={'/'}>Home</Link>
-          <Link className={`text-white font-bold rounded-md px-2 hover:bg-zinc-800 ${pathname === "/about" ? 'text-gray-500': ''} `} href={'/about'}>About</Link>
-        </div>
+          <nav>
+            <Link className={`text-white font-bold rounded-md px-2 hover:bg-zinc-800 ${pathname === "/" ? 'text-gray-500': ''} `} href={'/'}>Home</Link>
+            <Link className={`text-white font-bold rounded-md px-2 hover:bg-zinc-800 ${pathname === "/about" ? 'text-gray-500': ''} `} href={'/about'}>About</Link>
 
+          </nav>
+        </div>
 
     </header>
   )
