418d655a9ab7cb28bcba11accea38d4e3cd18fb7
alerodriguezn <navarrocmo@gmail.com>
2023-03-04 22:39:16 -0600

    Update Navbar

--- components/navbar.js
+++ components/navbar.js
@@ -5,18 +5,15 @@ import Link from "next/link"
 //TODO: change Link List
 const Navbar = () => {
   const pathname = usePathname();
-  console.log(pathname)
   return (
     <header className="mt-8 flex flex-col justify-center items-center md:flex-row md:justify-between w-full">
         <h2 className="text-white font-bold mb-3 md:mb-0"> <span className="text-[#6bd968]">{'{ '}</span> Ale Rodríguez<span className="text-[#6bd968]">{' }'}</span></h2>
         <div className="flex md:gap-4 gap-1">
-          <Link className={`text-white font-bold rounded-md px-2 hover:bg-zinc-800 ${pathname === "/" ? 'text-gray-500': ''} `} href={'/'}>Home</Link>
-          <Link className={`text-white font-bold rounded-md px-2 hover:bg-zinc-800 ${pathname === "/about" ? 'text-gray-500': ''} `} href={'/about'}>About</Link>
+          <Link className={`text-white font-bold rounded-md px-2 hover:bg-zinc-800 ${pathname === "/" ? 'text-gray-500': ''} `} href='/'>Home</Link>
+          <Link className={`text-white font-bold rounded-md px-2 hover:bg-zinc-800 ${pathname === "/about" ? 'text-gray-500': ''} `} href='/about'>About</Link>
         </div>
-
-
     </header>
   )
 }
 
-export default Navbar
+export default Navbar;
