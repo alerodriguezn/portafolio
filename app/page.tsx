8437768b4af69e2f6adc2243c12a8d841f4f45c6
alerodriguezn <navarrocmo@gmail.com>
2023-06-25 20:20:00 -0600

    Fix url

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ async function getRepos(): Promise<RepositoryProps[]> {
 
     return data;
   } catch (error) {
-    console.error(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/repos`, error);
+    console.error(`http://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/repos`, error);
     throw new Error('Error fetching data');
   }
 }
