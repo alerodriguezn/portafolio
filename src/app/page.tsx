810ac7d419fc1fbd54e1d58953f7b8655f61fb53
alerodriguezn <navarrocmo@gmail.com>
2023-12-12 23:08:05 -0600

    Logos svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import { Navbar } from "../components/Navbar";
 import photo from "@/public/img/photo.jpeg";
+import Link from "next/link";
+import { Github } from "../components/logos/github";
+import { Linkedin } from "../components/logos/linkedin";
+import { X } from "@/components/logos/x";
 
 export default function Home() {
   return (
@@ -14,24 +18,37 @@ export default function Home() {
         <Navbar />
       </header>
       <main className="w-3/4 flex justify-center items-center mt-4 flex-col">
-        <div className="w-full sm:w-2/3 flex border-4 rounded-md border-slate-800 p-2">
-          <div className="w-3/4 flex justify-center items-center">
-            <p className="text-sm text-justify">
-              Prospective Computer Engineering student with expertise in
-              frontend and backend development. Demonstrated skills through hands-on projects.
-            </p>
+        <div className="w-full sm:w-2/3 flex flex-col justify-center items-center border-4 rounded-md border-slate-900">
+          <div className="flex p-2">
+            <div className="w-3/4 flex justify-center items-center">
+              <p className="text-sm text-justify">
+                Prospective Computer Engineering student with expertise in
+                frontend and backend development. Demonstrated skills through
+                hands-on projects.
+              </p>
+            </div>
+            <div className="w-1/2 flex justify-center items-center">
+              <Image
+                src={photo}
+                alt="profile photo"
+                width={75}
+                height={75}
+                className="rounded-full object-cover contrast-100"
+              />
+            </div>
           </div>
-          <div className="w-1/2 flex justify-center items-center">
-            <Image
-              src={photo}
-              alt="profile photo"
-              width={75}
-              height={75}
-              className="rounded-full object-cover contrast-100"
-            />
+          <div className="w-full flex justify-center gap-4 mb-2">
+            <Link href="https://github.com/alerodriguezn">
+              <Github />
+            </Link>
+            <Link href="https://github.com/alerodriguezn">
+              <Linkedin />
+            </Link>
+            <Link href="https://github.com/alerodriguezn">
+              <X />
+            </Link>
           </div>
         </div>
-        
       </main>
     </>
   );
